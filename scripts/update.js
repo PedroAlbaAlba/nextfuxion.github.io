4daa14386f5371f69ba331aea96003cfd6eeb1f1
PedroAlbaAlba <120492245+PedroAlbaAlba@users.noreply.github.com>
2026-05-23 21:06:18 -0500

    Create update.js

---
+++ scripts/update.js
@@ -0,0 +1,12 @@
+console.log(
+"Preparando lector FuXion Colombia"
+);
+
+console.log(
+"Pais por defecto: es-CO"
+);
+
+console.log(
+"Fuente:",
+"https://ifuxion.com/giovannaastridrangelfarfan/enrollment/products"
+);
