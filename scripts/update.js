39cf66a12212f26328c1df4df70e60b3d9d93230
PedroAlbaAlba <120492245+PedroAlbaAlba@users.noreply.github.com>
2026-05-23 21:53:39 -0500

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,8 +1,7 @@
-const fs = require("fs");
 const axios = require("axios");
-const cheerio = require("cheerio");
+const fs = require("fs");
 
-async function actualizar() {
+async function revisar(){
 
 const url =
 "https://ifuxion.com/giovannaastridrangelfarfan/enrollment/products";
@@ -18,100 +17,15 @@ Cookie:
 }
 );
 
-const $ =
-cheerio.load(
+fs.writeFileSync(
+"pagina.html",
 respuesta.data
 );
 
-const productos = [];
-
-/* buscar tarjetas comunes */
-
-$("img").each((i,el)=>{
-
-const imagen =
-$(el).attr("src");
-
-const texto =
-$(el)
-.parent()
-.text()
-.trim();
-
-if(
-imagen &&
-texto.length > 3
-){
-
-productos.push({
-
-nombre:
-texto
-.substring(
-0,
-80
-),
-
-precio:
-"$0 COP",
-
-categoria:
-"FuXion",
-
-imagen:
-imagen.startsWith("http")
-? imagen
-: "https://ifuxion.com" +
-imagen,
-
-link:
-"https://ifuxion.com/GIOVANNAASTRIDRANGELFARFAN"
-
-});
-
-}
-
-});
-
-/* evitar vacío */
-
-if(
-productos.length===0
-){
-
-productos.push({
-
-nombre:
-"Sin productos detectados",
-
-precio:
-"$0 COP",
-
-categoria:
-"Prueba",
-
-imagen:"",
-link:
-"https://ifuxion.com/GIOVANNAASTRIDRANGELFARFAN"
-
-});
-
-}
-
-fs.writeFileSync(
-"productos.json",
-JSON.stringify(
-productos,
-null,
-2
-)
-);
-
 console.log(
-"Productos:",
-productos.length
+"HTML guardado"
 );
 
 }
 
-actualizar();
+revisar();
