3fcc9b6975d24f2914b5629645092792f9f3a8b4
PedroAlbaAlba <120492245+PedroAlbaAlba@users.noreply.github.com>
2026-05-23 20:59:42 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+  country: "es-CO",
+  storeUrl:
+    "https://ifuxion.com/giovannaastridrangelfarfan/enrollment/products",
+
+  buyUrl:
+    "https://ifuxion.com/GIOVANNAASTRIDRANGELFARFAN",
+
+  whatsapp:
+    "https://wa.me/573002117268?text=Hola,%20quiero%20comprar%20productos%20FuXion%20desde%20NextFuXion.",
+
+  cookie:
+    "FuXionSiteCulture=es-CO"
+};
