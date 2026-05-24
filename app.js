b2246dd5e207d70b41d823d86c124ecffcd37187
PedroAlbaAlba <120492245+PedroAlbaAlba@users.noreply.github.com>
2026-05-23 20:45:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 
+async function cargarProductos(){
+
+const res = await fetch("productos.json");
+
+const productos = await res.json();
+
+const contenedor =
+document.getElementById(
+"productos"
+);
+
+if(!contenedor) return;
+
+productos.forEach(p=>{
+
+contenedor.innerHTML += `
+<div class="card">
+
+<img src="${p.imagen}">
+
+<h3>${p.nombre}</h3>
+
+<p>${p.categoria}</p>
+
+<h4>${p.precio}</h4>
+
+<a href="${p.link}">
+Comprar
+</a>
+
+</div>
+`;
+
+});
+
+}
+
+cargarProductos();
