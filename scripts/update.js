b56a06d35fa4e7b795f6291ebb87cdb2c4ecef08
PedroAlbaAlba <120492245+PedroAlbaAlba@users.noreply.github.com>
2026-05-23 23:11:08 -0500

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,10 +1,47 @@
-try {
+const fs = require("fs");
 
-console.log("Inicio");
+async function probar(){
 
-const axios = require("axios");
+try{
 
-console.log("Axios cargado");
+const axios =
+require("axios");
+
+console.log(
+"Iniciando descarga"
+);
+
+const respuesta =
+await axios.get(
+"https://ifuxion.com/giovannaastridrangelfarfan/enrollment/products",
+{
+headers:{
+Cookie:
+"FuXionSiteCulture=es-CO",
+"User-Agent":
+"Mozilla/5.0"
+},
+timeout:30000
+}
+);
+
+console.log(
+"Descarga OK"
+);
+
+console.log(
+"Tamaño HTML:",
+respuesta.data.length
+);
+
+fs.writeFileSync(
+"pagina.html",
+respuesta.data
+);
+
+console.log(
+"HTML guardado"
+);
 
 process.exit(0);
 
@@ -12,11 +49,17 @@ process.exit(0);
 catch(error){
 
 console.error(
-"CARGA FALLIDA:"
+"ERROR REAL:"
 );
 
-console.error(error);
+console.error(
+error.message
+);
 
 process.exit(1);
 
 }
+
+}
+
+probar();
