a96fe9d863148485209ee70692333f8098462ac4
PedroAlbaAlba <120492245+PedroAlbaAlba@users.noreply.github.com>
2026-05-23 21:12:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,20 +5,66 @@ document.getElementById(
 "productos"
 );
 
-contenedor.innerHTML = `
+contenedor.innerHTML =
+"<p>Cargando productos...</p>";
+
+try{
+
+const respuesta =
+await fetch(
+"productos.json"
+);
+
+const productos =
+await respuesta.json();
+
+contenedor.innerHTML = "";
+
+productos.forEach(p=>{
+
+contenedor.innerHTML += `
+
 <div class="card">
-<h3>Sincronización FuXion Colombia</h3>
 
-<p>Pais: Colombia</p>
+<img
+src="${p.imagen}"
+alt="${p.nombre}">
 
-<p>Estado: Preparando conexión</p>
+<h3>
+${p.nombre}
+</h3>
 
-<a href="${CONFIG.buyUrl}">
+<p>
+${p.categoria}
+</p>
+
+<h4>
+${p.precio}
+</h4>
+
+<a
+href="${p.link}"
+target="_blank">
+
 Comprar
+
 </a>
 
 </div>
+
 `;
+
+});
+
+}catch(error){
+
+contenedor.innerHTML = `
+<p>
+Error cargando catálogo
+</p>
+`;
+
+}
 
 }
 
