f1c292260c9689eb08f23770cf1ca8dba023f19b
nextfuxion <120492245+nextfuxion@users.noreply.github.com>
2026-05-28 12:35:26 -0500

    Create carrito.js

---
+++ carrito.js
@@ -0,0 +1,241 @@
+let carrito =
+JSON.parse(
+localStorage.getItem("carrito")
+)||[];
+
+function precioNumero(precio){
+
+return Number(
+
+String(precio)
+
+.replace("$","")
+
+.replace(/\./g,"")
+
+.replace(",", ".")
+
+.replace(/[^\d.]/g,"")
+
+)||0;
+
+}
+
+function cargarCarrito(){
+
+const contenedor =
+document.getElementById(
+"carritoProductos"
+);
+
+let html = "";
+
+let total = 0;
+
+carrito.forEach(
+(producto,index)=>{
+
+const subtotal =
+
+precioNumero(
+producto.precio
+)
+
+*
+
+producto.cantidad;
+
+total += subtotal;
+
+html += `
+
+<div class="card">
+
+<img
+src="${producto.imagen}"
+style="width:120px;">
+
+<h3>
+
+${producto.nombre}
+
+</h3>
+
+<p>
+
+${producto.precio}
+
+</p>
+
+<p>
+
+Cantidad:
+${producto.cantidad}
+
+</p>
+
+<button
+onclick="sumar(${index})">
+
+➕
+
+</button>
+
+<button
+onclick="restar(${index})">
+
+➖
+
+</button>
+
+<button
+onclick="eliminar(${index})">
+
+🗑️
+
+</button>
+
+</div>
+
+`;
+
+});
+
+contenedor.innerHTML =
+html;
+
+document.getElementById(
+"totalCarrito"
+).innerHTML =
+
+"Total: $" +
+
+total.toLocaleString(
+"es-CO"
+);
+
+}
+
+function guardar(){
+
+localStorage.setItem(
+"carrito",
+JSON.stringify(carrito)
+);
+
+cargarCarrito();
+
+}
+
+function sumar(i){
+
+carrito[i].cantidad++;
+
+guardar();
+
+}
+
+function restar(i){
+
+if(
+carrito[i].cantidad>1
+){
+
+carrito[i].cantidad--;
+
+}else{
+
+carrito.splice(i,1);
+
+}
+
+guardar();
+
+}
+
+function eliminar(i){
+
+carrito.splice(i,1);
+
+guardar();
+
+}
+
+function enviarPedido(){
+
+const nombre =
+document.getElementById(
+"nombre"
+).value;
+
+const telefono =
+document.getElementById(
+"telefono"
+).value;
+
+const correo =
+document.getElementById(
+"correo"
+).value;
+
+const ciudad =
+document.getElementById(
+"ciudad"
+).value;
+
+const direccion =
+document.getElementById(
+"direccion"
+).value;
+
+const observaciones =
+document.getElementById(
+"observaciones"
+).value;
+
+let mensaje =
+
+`*Pedido NextFuXion*%0A%0A`;
+
+mensaje +=
+`Cliente: ${nombre}%0A`;
+
+mensaje +=
+`Teléfono: ${telefono}%0A`;
+
+mensaje +=
+`Correo: ${correo}%0A`;
+
+mensaje +=
+`Ciudad: ${ciudad}%0A`;
+
+mensaje +=
+`Dirección: ${direccion}%0A`;
+
+mensaje +=
+`Observaciones: ${observaciones}%0A%0A`;
+
+mensaje +=
+`*Productos:*%0A`;
+
+carrito.forEach(
+p=>{
+
+mensaje +=
+
+`${p.cantidad} x ${p.nombre}%0A`;
+
+}
+);
+
+window.open(
+
+`https://wa.me/573002117268?text=${mensaje}`,
+
+"_blank"
+
+);
+
+}
+
+cargarCarrito();
